d12217d2dc6ae3e76a7516c1cd4914d2705f5896
김진혁 <gandolkim@gmail.com>
2020-10-22 15:50:01 +0900

    sass-loader added

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -22,6 +22,16 @@ module.exports = {
           'vue-style-loader',
           'css-loader'
         ]
+      },
+      // this will apply to both plain `.scss` files
+      // AND `<style lang="scss">` blocks in `.vue` files
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   },
