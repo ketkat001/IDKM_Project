5836ae7f50fabe55500183f7feb4a50ca426c6ca
김진혁 <gandolkim@gmail.com>
2020-10-27 11:00:45 +0900

    S03P31A504-11 | navbar added

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import MovieList from './views/MovieList.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +12,16 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/movielist',
+    name: 'MovieList',
+    component: MovieList
+  }
 ]
 
 const router = new VueRouter({
