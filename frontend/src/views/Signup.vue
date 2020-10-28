4a122832b6e161bd49302f50dd4fa1ba288d1d1d
gandolkim <gandolkim@gmail.com>
2020-10-28 13:16:31 +0900

    S03P31A504-5 | Signup styling updated

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,22 +1,73 @@
 <template>
   <div class="signup-page">
-    This is signup.vue
+    <div class="signup-box">
+      <h2>우리 사이트에 처음 오셨군요!</h2>
+      <div class="input-box">
+        <label for="signup-id"> 아이디</label>
+        <input
+          type="text"
+          label="id"
+          name="signup-id"
+          placeholder="아이디를 입력하세요"
+          required="아이디를 입력해 주세요."
+        />
+      </div>
+      <div class="input-box">
+        <label for="signup-password">비밀번호</label>
+        <input
+          type="password"
+          label="password"
+          name="signup-password"
+          placeholder="비밀번호를 입력하세요"
+          required="비밀번호를 입력해 주세요."
+        />
+      </div>
+      <div class="input-box">
+        <label for="signup-password">비밀번호 확인</label>
+        <input
+          type="password"
+          label="password"
+          name="signup-password"
+          placeholder="비밀번호를 한번 더 입력하세요"
+          required="비밀번호를 한번 더 입력하셔야 합니다."
+        />
+      </div>
+      <div class="input-box">
+        <label for="signup-password">이름</label>
+        <input
+          type="password"
+          label="password"
+          name="signup-password"
+          placeholder="이메일 주소를 입력하세요"
+          required="이메일을 입력해 주세요."
+        />
+      </div>
+      <div class="input-box">
+        <label for="signup-password">이메일</label>
+        <input
+          type="password"
+          label="password"
+          name="signup-password"
+          placeholder="이메일 주소를 입력하세요"
+          required="이메일을 입력해 주세요."
+        />
+      </div>
+
+      <input type="submit" value="회원 가입" />
+    </div>
   </div>
 </template>
 
 <script>
-import "@/assets/css/signup.scss"
+import "@/assets/css/signup.scss";
 
 export default {
-  name: 'Signup',
+  name: "Signup",
   created() {
     // email, id, password RegExp needed
   },
   data() {
-    return {
-      
-    }
-  }
-
-}
+    return {};
+  },
+};
 </script>
