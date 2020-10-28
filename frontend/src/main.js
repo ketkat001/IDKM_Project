f1a280e80f1f88d717873e54683a80300f277f81
ketkat001 <ketkat001@gmail.com>
2020-10-28 10:15:49 +0900

    S03P31A504-14 | front-mainpage has updated

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routes'
 import store from "./vuex/index"
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,6 @@ Vue.config.productionTip = false
 new Vue({
   store,
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
