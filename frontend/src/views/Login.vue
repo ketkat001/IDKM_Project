48889fb977dc9120bee2411a9edd01678515bb4d
gandolkim <gandolkim@gmail.com>
2020-10-28 14:29:49 +0900

    S03P31A504-18 | login styling updated

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -2,24 +2,28 @@
   <div class="login-page">
     <div class="login-box">
       <h1>로그인</h1>
-      <label for="login-id">아이디</label>
-      <input
-        type="text"
-        label="id"
-        name="login-id"
-        placeholder="아이디를 입력하세요"
-        required="아이디를 입력해 주세요."
-      />
-      <label for="login-password">비밀번호</label>
-      <input
-        type="password"
-        label="password"
-        name="login-password"
-        placeholder="비밀번호를 입력하세요"
-        required="비밀번호를 입력해 주세요."
-      />
+      <div class="input-box">
+        <label for="login-id">아이디</label>
+        <input
+          type="text"
+          label="id"
+          name="login-id"
+          placeholder="아이디를 입력하세요"
+          required="아이디를 입력해 주세요."
+        />
+      </div>
+      <div class="input-box">
+        <label for="login-password">비밀번호</label>
+        <input
+          type="password"
+          label="password"
+          name="login-password"
+          placeholder="비밀번호를 입력하세요"
+          required="비밀번호를 입력해 주세요."
+        />
+      </div>
       <div class="submit-box">
-        <input type="submit" value="제 출" />
+        <input type="submit" value="로그인하기" />
         <span class="warning-message"
           >아이디 또는 비밀번호를 입력해주세요!</span
         >
