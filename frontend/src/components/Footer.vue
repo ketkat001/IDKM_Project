1f01504b23e155dcda8227a8347e5d1184a3064c
gandolkim <gandolkim@gmail.com>
2020-10-28 15:58:12 +0900

    S03P31A504-21 | footer font and color updated

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,10 +1,12 @@
 <template>
-  <div id="footer">
+  <div class="footer">
     <h4>Created by ESC. Powered by Vue.</h4>
   </div>
 </template>
 
 <script>
+import "@/assets/css/footer.scss"
+
 export default {
   name: "Footer"
 };
--- frontend/src/components/Movies/MovieSearch.vue
+++ frontend/src/components/Movies/MovieSearch.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="movie-search">
     <h2>원하는 결과를 아직 찾지 못했나요?</h2>
-    
+
   </div>
 </template>
 
 <script>
 export default {
+  name: "MovieSearch"
 
 }
 </script>
