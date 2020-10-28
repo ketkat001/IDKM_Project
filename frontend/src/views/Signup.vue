95f70f0f1a185dff1d6cf2c4af4d46863513b95a
gandolkim <gandolkim@gmail.com>
2020-10-28 13:46:53 +0900

    S03P31A504-19 | logic added

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -6,6 +6,7 @@
         <label for="signup-id"> 아이디</label>
         <input
           type="text"
+          v-model="signupData.userid"
           name="signup-id"
           placeholder="아이디를 입력하세요"
           required="아이디를 입력해 주세요."
@@ -15,6 +16,7 @@
         <label for="signup-password">비밀번호</label>
         <input
           type="password"
+          v-model="signupData.password"
           name="signup-password"
           placeholder="비밀번호를 입력하세요"
           required="비밀번호를 입력해 주세요."
@@ -24,6 +26,7 @@
         <label for="signup-password-again">비밀번호 확인</label>
         <input
           type="password"
+          v-model="passwordCheck"
           name="signup-password-again"
           placeholder="비밀번호를 한번 더 입력하세요"
           required="비밀번호를 한번 더 입력하셔야 합니다."
@@ -33,6 +36,7 @@
         <label for="signup-username">이름</label>
         <input
           type="text"
+          v-model="signupData.username"
           name="signup-username"
           placeholder="이름을 입력하세요"
           required="이메일을 입력해 주세요."
@@ -42,6 +46,7 @@
         <label for="signup-email">이메일</label>
         <input
           type="email"
+          v-model="signupData.email"
           name="signup-email"
           placeholder="이메일 주소를 입력하세요"
           required="이메일을 입력해 주세요."
@@ -62,7 +67,15 @@ export default {
     // email, id, password RegExp needed
   },
   data() {
-    return {};
+    return {
+      passwordCheck: '',
+      signupData: {
+        userid: '',
+        username: '',
+        password: '',
+        email: ''
+      }
+    }
   },
 };
 </script>
