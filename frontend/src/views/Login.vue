e0a46b80313218acfad39c9d66b2b4358bc749ce
gandolkim <gandolkim@gmail.com>
2020-10-28 14:25:40 +0900

    S03P31A504-18 | login logic added

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -3,11 +3,32 @@
     <div class="login-box">
       <h1>로그인</h1>
       <label for="login-id">아이디</label>
-      <input type="text" label="id" name="login-id" placeholder="아이디를 입력하세요" required="아이디를 입력해 주세요."/>
+      <input
+        type="text"
+        label="id"
+        name="login-id"
+        placeholder="아이디를 입력하세요"
+        required="아이디를 입력해 주세요."
+      />
       <label for="login-password">비밀번호</label>
-      <input type="password" label="password" name="login-password" placeholder="비밀번호를 입력하세요" required="비밀번호를 입력해 주세요."/>
-      <input type="submit" value="제 출" />
-      <p><span>아직 회원이 아니신가요?   <a href="/signup">회원가입 하러가기</a></span></p>
+      <input
+        type="password"
+        label="password"
+        name="login-password"
+        placeholder="비밀번호를 입력하세요"
+        required="비밀번호를 입력해 주세요."
+      />
+      <div class="submit-box">
+        <input type="submit" value="제 출" />
+        <span class="warning-message"
+          >아이디 또는 비밀번호를 입력해주세요!</span
+        >
+      </div>
+      <p>
+        <span
+          >아직 회원이 아니신가요? <a href="/signup">회원가입 하러가기</a></span
+        >
+      </p>
     </div>
   </div>
 </template>
@@ -16,7 +37,15 @@
 import "@/assets/css/login.scss";
 
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      loginData: {
+        userid: "",
+        password: "",
+      },
+    };
+  },
 };
 </script>
 
