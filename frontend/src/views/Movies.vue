fe4e5703b47faff20daf8b827eb88d40bdc992d7
gandolkim <gandolkim@gmail.com>
2020-10-28 16:21:39 +0900

    S03P31A504-21 | movies components details updated

--- frontend/src/views/Movies.vue
+++ frontend/src/views/Movies.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="movie-list">
     <MovieSearch />
+    <MovieResult />
+    <MovieSuggest />
   </div>
 </template>
 
 <script>
 import "@/assets/css/movieList.scss"
+import MovieResult from "@/components/Movies/MovieResult.vue"
 import MovieSearch from "@/components/Movies/MovieSearch.vue"
+import MovieSuggest from "@/components/Movies/MovieSuggest.vue"
 
 export default {
   name: "MovieList",
   components: {
-    MovieSearch
+    MovieResult,
+    MovieSearch,
+    MovieSuggest
   }
 
 }
