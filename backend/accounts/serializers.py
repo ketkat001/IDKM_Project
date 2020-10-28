836a1dc90a66b8c63c2387e563dbd8fed740ee03
최정원 <jungwon9156@khu.ac.kr>
2020-10-28 16:34:06 +0900

    S03P31A504-13|added|회원가입, 로그인

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import User
-# from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate
 
 
 # 회원가입
@@ -15,3 +15,29 @@ class CreateUserSerializer(serializers.ModelSerializer):
             validated_data['email'], validated_data['nickname'], validated_data['password']
         )
         return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    nickname = serializers.CharField()
+    name = serializers.CharField()
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'nickname', 'name', 'profile_image')
+
+
+# 로그인
+class LoginUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+    
+    def validate(self, data):
+        user = authenticate(**data)
+        print(user)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Unable to login with provided credentials.")
+
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework import permissions, generics
-from .serializers import CreateUserSerializer
-from knox.models import AuthToken
+from .serializers import CreateUserSerializer, UserSerializer, LoginUserSerializer
+
 # Create your views here.
 
-
+#회원가입
 class RegistrationAPI(generics.GenericAPIView):
     serializer_class = CreateUserSerializer
 
@@ -12,12 +13,27 @@ class RegistrationAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        token = AuthToken.objects.create(user)[1]
         return Response (
             {
                 'user' : UserSerializer(
                     user, context = self.get_serializer_context()
                 ).data,
-                'token': token,
+            }
+        )
+
+#로그인
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response(
+            {
+                'user': UserSerializer(
+                    user, context=self.get_serializer_context()
+                ).data,
+
             }
         )
