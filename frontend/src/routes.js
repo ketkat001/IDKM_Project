fef62f35e160cbd447738f3e53e4c05e377f69c6
김진혁 <gandolkim@gmail.com>
2020-11-01 18:07:43 +0900

    S03P31A504-29 | lazy loading added in routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
-import Signup from './views/Signup.vue'
 import Movies from './views/Movies.vue'
+
+const Signup = () => import(/* webpackChunkName: "Signup" */ './views/Signup.vue')
 
 Vue.use(VueRouter)
 
