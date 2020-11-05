1aef6e7f83af617837d22cb82d92378ae790b657
김진혁 <gandolkim@gmail.com>
2020-11-05 09:40:18 +0900

    S03P31A504-35|signup component updated

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -3,22 +3,33 @@
     <div class="signup-box">
       <h2>우리 사이트에 처음 오셨군요!</h2>
       <div class="input-box">
-        <label for="signup-email">이메일</label>
+        <label for="signup-email"
+          >이메일<span v-show="!isEmailEmpty" class="warning-message"
+            >이메일을 입력해주세요!!</span
+          ></label
+        >
         <input
           type="email"
           v-model="signupData.email"
           name="signup-email"
           placeholder="이메일 주소를 입력하세요"
           required="이메일을 입력해 주세요."
+          @focus="emailFlag = true"
         />
       </div>
       <div class="input-box">
-        <label for="signup-password">비밀번호</label>
+        <label for="signup-password"
+          >비밀번호
+          <span v-show="!isPasswordMatch" class="warning-message"
+            >비밀번호를 입력해 주세요!</span
+          >
+        </label>
         <input
           type="password"
           v-model.lazy.trim="signupData.password"
           name="signup-password"
           placeholder="비밀번호를 입력하세요"
+          @focus="passwordFlag = true"
           required="비밀번호를 입력해 주세요."
         />
       </div>
@@ -38,20 +49,31 @@
         />
       </div>
       <div class="input-box">
-        <label for="signup-username">이름</label>
+        <label for="signup-username"
+          >이름<span v-show="!isUsernameEmpty" class="warning-message"
+            >이름을 입력해 주세요!</span
+          ></label
+        >
         <input
           type="text"
           v-model="signupData.username"
           name="signup-username"
           placeholder="이름을 입력하세요"
           required="이메일을 입력해 주세요."
+          @focus="usernameFlag = true"
         />
       </div>
-      
 
       <div class="submit-box">
-        <input type="submit" value="회원 가입" :disabled="!hasAllProperty" @click="signup(signupData)"/>
-        <span v-show="!hasAllProperty" class="warning-message">모든 정보를 입력해주세요!</span>
+        <input
+          type="submit"
+          value="회원 가입"
+          :disabled="!hasAllProperty"
+          @click="signup(signupData)"
+        />
+        <span v-show="!hasAllProperty" class="warning-message"
+          >모든 정보를 입력해주세요!</span
+        >
       </div>
     </div>
   </div>
@@ -59,7 +81,7 @@
 
 <script>
 import "@/assets/css/views/signup.scss";
-import { mapActions } from "vuex"
+import { mapActions } from "vuex";
 
 export default {
   name: "Signup",
@@ -67,26 +89,35 @@ export default {
     // email, id, password RegExp needed
   },
   computed: {
+    isEmailEmpty() {
+      return this.emailFlag && !!this.signupData.email;
+    },
+    isUsernameEmpty() {
+      return this.usernameFlag && !!this.signupData.username;
+    },
     isPasswordEmpty() {
-      return !this.signupData.password
+      return !this.signupData.password;
     },
     isPasswordMatch() {
-      return this.passwordCheck === this.signupData.password
+      return this.passwordCheck === this.signupData.password;
     },
     hasAllProperty() {
       return Boolean(
-        this.signupData.username !== '' && 
-        this.signupData.password !== '' && 
-        this.signupData.email !== '' &&
-        this.isPasswordMatch
-      )
-    }
+        this.signupData.username !== "" &&
+          this.signupData.password !== "" &&
+          this.signupData.email !== "" &&
+          this.isPasswordMatch
+      );
+    },
   },
   methods: {
-    ...mapActions("accounts", ["signup"])
+    ...mapActions("accounts", ["signup"]),
   },
   data() {
     return {
+      emailFlag: false,
+      passwordFlag: false,
+      usernameFlag: false,
       passwordCheck: "",
       signupData: {
         username: "",
