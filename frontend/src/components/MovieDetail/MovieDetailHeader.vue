1dfef3f4e0788e8d51f834ec38a7ddbcd11af26d
ketkat001 <ketkat001@gmail.com>
2020-11-10 10:30:30 +0900

    S03P31A504-48 | Movie detail page movie-header has updated

--- frontend/src/components/MovieDetail/MovieDetailHeader.vue
+++ frontend/src/components/MovieDetail/MovieDetailHeader.vue
@@ -7,7 +7,25 @@
         <div class="movie-info-content">
           <div class="movie-info-content-wrap">
             <div class="movie-info-item-data">
-              
+              <span class="movie-year">2020</span>
+              <span class="movie-title">몰라</span>
+              <span class="movie-genre">
+                <a href="">액션</a>
+                <a href="">드라마</a>
+                <a href="">힐링</a>
+                <a href="">망함</a>
+              </span>
+            </div>
+            <div class="movie-buttons">
+              <v-btn icon>
+                <span class="iconify" data-icon="mdi-youtube" data-inline="false"></span>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
             </div>
           </div>
         </div>
