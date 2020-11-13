eeb4f831b36eaf89bb95103831e888f8edc39ec8
최정원 <jungwon9156@khu.ac.kr>
2020-11-13 17:23:02 +0900

    추천업뎃
    Former-commit-id: 3ca6a450a170af962d3196fb3b480a229fc600c0

--- backend/movies/recommend.py
+++ backend/movies/recommend.py
@@ -3,7 +3,7 @@ import numpy as np
 from pandas import Series, DataFrame
 import pymysql
 from konlpy.tag import Kkma
-
+import json
 
 def recommend_sys(searchword):
     kkma = Kkma()
@@ -23,43 +23,57 @@ def recommend_sys(searchword):
     result = cursor.fetchall()
     result = pd.DataFrame(result)
 
+    sql2 = "select * from mls;"
+    cursor.execute(sql2)
+    result2 = cursor.fetchall()
+    result2 = pd.DataFrame(result2)
+
     sql3 = "select * from movies_movie_tagdatas;"
     cursor.execute(sql3)
     result3 = cursor.fetchall()
     result3 = pd.DataFrame(result3)
-
     result3['weight'] = 1
 
-    mydict = pd.DataFrame()
-    D = pd.DataFrame(mydict)
-
+    D = []
     words= kkma.nouns(searchword)
-    print(words)
+
     for w in words:
-        words_s = result[result['tags'] == w]
-        D = pd.concat([D, words_s], ignore_index=True)
+        words_s = result[result['tags'] == w]['id']
+        D.extend(words_s.values)
     if len(D) == 0:
         return []
     else:
-        D = D.drop('tags', axis=1)
-        D.columns = ['tagdatas_id']
-        D['movie_id'] = 1
-        D['weight'] = 1
+        ml = list(result2['0'].values)
+        mov_l, score_l = list(), list()
+        for mnum in ml:
+            score = 0
+            tgd = list(result3[result3['movie_id'] == mnum]['tagdatas_id'].values)
+            for dn in D:
+                if dn in tgd:
+                    score += 1
+            
+            mov_l.append(mnum)
+            score_l.append(score)
+        data_df1 = pd.DataFrame({'movie_id': mov_l, 'score': score_l})
+        df1 = data_df1.sort_values(by=['score'], ascending=False)
+        df1 = df1[1:21]
+        df2 = list(df1['movie_id'].values)
 
-        result3 = pd.concat([result3, D], ignore_index=True)
+        return df2
 
-        data_df1 = result3.pivot_table(index='movie_id', columns='tagdatas_id', values='weight')
-        data_df1.fillna(0, inplace=True)
-        # print(data_df1)
-        m1 = Series(data_df1.loc[1])
 
-        # print(m1)
-        data_df2 = data_df1.mul(m1, axis=1)
-        data_df2 = data_df2.sum(axis = 1)
-        data_df2 = data_df2.sort_values(ascending=False)
-        df3 = data_df2[1:21]
-        df4 = [i for i in df3.index]
-        return df4
+        # data_df1 = result3.pivot_table(index='movie_id', columns='tagdatas_id', values='weight')
+        # data_df1.fillna(0, inplace=True)
+        # # print(data_df1)
+        # m1 = Series(data_df1.loc[1])
+
+        # # print(m1)
+        # data_df2 = data_df1.mul(m1, axis=1)
+        # data_df2 = data_df2.sum(axis = 1)
+        # data_df2 = data_df2.sort_values(ascending=False)
+        # df3 = data_df2[1:21]
+        # df4 = [i for i in df3.index]
+        # return
 
 
 def recommend_sys2(user_pk):
@@ -106,6 +120,3 @@ def recommend_sys2(user_pk):
         df3 = data_df2[1:21]
         df4 = [i for i in df3.index]
         return df4
-
-
-
